7e116eba0ba1b0cce71ffde606e52e765fd09914
Trigalo27 <internick27@gmail.com>
2021-02-23 10:54:47 -0600

    commit a devlogger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogService } from './services/log.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponent,
+    LogsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
